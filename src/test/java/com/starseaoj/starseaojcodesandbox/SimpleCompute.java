92f27a5f14a58965d18e7925c2f533c4d9388706
guiyi <1677852098@qq.com>
2024-08-13 01:20:55 +0800

    测试命令行编译运行

---
+++ src/test/java/com/starseaoj/starseaojcodesandbox/SimpleCompute.java
@@ -0,0 +1,13 @@
+/**
+ * @author guiyi
+ * @Date 2024/8/13 上午12:33:03
+ * @ClassName com.starseaoj.starseaojcodesandbox.SimpleCompute
+ * @function --> 测试终端命令行编译运行，注意不能加包名
+ */
+public class SimpleCompute {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("结果为" + (a + b));
+    }
+}
