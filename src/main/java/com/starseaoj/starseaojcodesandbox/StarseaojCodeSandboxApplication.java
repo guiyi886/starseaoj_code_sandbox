4fd0857aa461321443a2338b2f6f39f99515f5d1
guiyi <1677852098@qq.com>
2024-08-13 00:06:58 +0800

    init

---
+++ src/main/java/com/starseaoj/starseaojcodesandbox/StarseaojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.starseaoj.starseaojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarseaojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarseaojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starseaoj/starseaojcodesandbox/StarseaojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starseaoj.starseaojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarseaojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
