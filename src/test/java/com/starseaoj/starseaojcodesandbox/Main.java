bc74dbb22a392d8e075a5b9da9ad315b84676a3d
guiyi <1677852098@qq.com>
2024-08-13 02:03:52 +0800

    改为Main

--- src/test/java/com/starseaoj/starseaojcodesandbox/Main.java
+++ src/test/java/com/starseaoj/starseaojcodesandbox/Main.java
@@ -1,10 +1,10 @@
 /**
  * @author guiyi
  * @Date 2024/8/13 上午12:33:03
- * @ClassName com.starseaoj.starseaojcodesandbox.SimpleCompute
+ * @ClassName com.starseaoj.starseaojcodesandbox.Main
  * @function --> 测试终端命令行编译运行，注意不能加包名
  */
-public class SimpleCompute {
+public class Main {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
