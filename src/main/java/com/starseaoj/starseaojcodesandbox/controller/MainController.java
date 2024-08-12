16aed2db8ce9710a953f84fcf419d9132a830005
guiyi <1677852098@qq.com>
2024-08-13 01:20:29 +0800

    从oj项目中移植相关类和接口

---
+++ src/main/java/com/starseaoj/starseaojcodesandbox/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.starseaoj.starseaojcodesandbox.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author guiyi
+ * @Date 2024/8/13 上午12:17:59
+ * @ClassName com.starseaoj.starseaojcodesandbox.controller.MainController
+ * @function -->
+ */
+@RestController
+public class MainController {
+    @GetMapping("ok")
+    public String ok() {
+        return "ok";
+    }
+}
