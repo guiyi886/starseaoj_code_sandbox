f19b2ada5d383ace84e56c5923aab134dd73b6e0
guiyi <1677852098@qq.com>
2024-08-13 22:17:18 +0800

    代码沙箱-异常情况-写文件植入木马

---
+++ src/main/java/com/starseaoj/starseaojcodesandbox/unsafe/WriteFileError.java
@@ -0,0 +1,27 @@
+package com.starseaoj.starseaojcodesandbox.unsafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * @author guiyi
+ * @Date 2024/8/13 下午10:07:57
+ * @ClassName com.starseaoj.starseaojcodesandbox.unsafe.WriteFileError
+ * @function --> 写文件，植入木马
+ */
+public class WriteFileError {
+    public static void main(String[] args) throws IOException {
+        String userDir = System.getProperty("user.dir");
+        String filePath = userDir + File.separator + "src/main/resources/木马程序.bat";
+
+        // 创建bat文件，将恶意代码如删除文件等写入
+        String errorProgram = "this is bad code";
+        Files.write(Paths.get(filePath), Arrays.asList(errorProgram));
+
+        // 使用终端命令运行bat文件...
+        System.out.println("运行bat文件");
+    }
+}
