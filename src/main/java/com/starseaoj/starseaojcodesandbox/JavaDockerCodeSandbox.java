ecb26a8b17987cb1b25e48e99bb79623ea02d02f
guiyi <1677852098@qq.com>
2024-08-17 21:11:17 +0800

    代码沙箱docker-数据卷挂载-待转远程开发

--- src/main/java/com/starseaoj/starseaojcodesandbox/JavaDockerCodeSandbox.java
+++ src/main/java/com/starseaoj/starseaojcodesandbox/JavaDockerCodeSandbox.java
@@ -2,6 +2,16 @@ package com.starseaoj.starseaojcodesandbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.Bind;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PullResponseItem;
+import com.github.dockerjava.api.model.Volume;
+import com.github.dockerjava.core.DockerClientBuilder;
 import com.starseaoj.starseaojcodesandbox.model.ExecuteCodeRequest;
 import com.starseaoj.starseaojcodesandbox.model.ExecuteCodeResponse;
 import com.starseaoj.starseaojcodesandbox.model.ExecuteMessage;
@@ -25,6 +35,8 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
     private static final String JAVA_CLASS_NAME = "Main.java";
 
     private static final String SECURITY_MANAGER_PATH = "D:\\JavaProjects\\starseaoj_code_sandbox\\src\\main\\resources\\security";
+
+    private static final Boolean FIRST_INIT = true;
 
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
@@ -63,7 +75,52 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
         }
 
         // 3.创建容器，复制文件到其中
-        
+        // 创建 Docker 客户端
+        DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://8.134.202.187:2375").build();
+
+        // 拉取镜像
+        String image = "openjdk:8-alpine";
+        if (FIRST_INIT) {
+            PullImageCmd pullImageCmd = dockerClient.pullImageCmd(image);
+            PullImageResultCallback pullImageResultCallback = new PullImageResultCallback() {
+                @Override
+                public void onNext(PullResponseItem item) {
+                    System.out.println("下载镜像：" + item.getStatus());
+                    super.onNext(item);
+                }
+            };
+            try {
+                pullImageCmd
+                        .exec(pullImageResultCallback)
+                        .awaitCompletion();
+            } catch (InterruptedException e) {
+                System.out.println("拉取镜像异常");
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("下载完成");
+
+        // 创建容器
+        CreateContainerCmd containerCmd = dockerClient.createContainerCmd(image);
+        HostConfig hostConfig = new HostConfig();
+        hostConfig.withMemory(100 * 1000 * 1000L);
+        hostConfig.withCpuCount(1L);
+        hostConfig.setBinds(new Bind(userCodeParentPath, new Volume("/app")));  // 文件路径映射
+
+        CreateContainerResponse createContainerResponse = containerCmd
+                .withHostConfig(hostConfig)
+                // .withNetworkDisabled(true)
+                // .withReadonlyRootfs(true)
+                .withAttachStdin(true)  // 与本地终端连接
+                .withAttachStderr(true)
+                .withAttachStdout(true)
+                .withTty(true)  // 创建交互终端
+                .exec();
+        System.out.println(createContainerResponse);
+        String containerId = createContainerResponse.getId();
+
+        // 启动容器
+        dockerClient.startContainerCmd(containerId).exec();
 
         // todo
         ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
@@ -99,7 +156,8 @@ public class JavaDockerCodeSandbox implements CodeSandbox {
         executeCodeRequest.setLanguage("java");
 
         // 获取java文件
-        String code = ResourceUtil.readStr("testCode/unsafe/ReadFileError.java", StandardCharsets.UTF_8);
+        // String code = ResourceUtil.readStr("testCode/unsafe/ReadFileError.java", StandardCharsets.UTF_8);
+        String code = ResourceUtil.readStr("testCode/Main.java", StandardCharsets.UTF_8);
         executeCodeRequest.setCode(code);
 
         // 调用代码沙箱
