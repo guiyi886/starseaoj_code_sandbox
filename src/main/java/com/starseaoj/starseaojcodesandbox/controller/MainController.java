f2db265f8a02eedf15155279379598c529d65541
guiyi <1677852098@qq.com>
2024-08-19 13:04:06 +0800

    代码沙箱-API调用鉴权

--- src/main/java/com/starseaoj/starseaojcodesandbox/controller/MainController.java
+++ src/main/java/com/starseaoj/starseaojcodesandbox/controller/MainController.java
@@ -3,12 +3,13 @@ package com.starseaoj.starseaojcodesandbox.controller;
 import com.starseaoj.starseaojcodesandbox.codesandbox.JavaNativeCodeSandboxNew;
 import com.starseaoj.starseaojcodesandbox.model.ExecuteCodeRequest;
 import com.starseaoj.starseaojcodesandbox.model.ExecuteCodeResponse;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author guiyi
@@ -18,15 +19,14 @@ import javax.annotation.Resource;
  */
 @RestController
 public class MainController {
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandboxNew javaNativeCodeSandboxNew;
-
-    @GetMapping("ok")
-    public String ok() {
-        return "ok";
-    }
-
+    
     /**
      * 调用代码沙箱执行代码
      *
@@ -34,7 +34,14 @@ public class MainController {
      * @return
      */
     @PostMapping("/executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest,
+                                           HttpServletRequest request, HttpServletResponse response) {
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
+
         if (executeCodeRequest == null) {
             throw new RuntimeException("请求参数为空");
         }
